f5d3fdd62086c1febfe8a9e1693f03c95156d085
amykeonucamp <amy@nucamp.co>
2020-07-05 12:47:40 -0700

    instructor comments. see notes that start with AMY INSTRUCTOR in CampsiteInfoComponent. please delete the instructor comments after fixing the bugs.

--- src/components/CampsiteInfoComponents.js
+++ src/components/CampsiteInfoComponents.js
@@ -1,56 +1,68 @@
-import React, { Component } from 'react';
-import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
+import React, { Component } from "react";
+import { Card, CardImg, CardText, CardBody, CardTitle } from "reactstrap";
 
 class CampsiteInfo extends Component {
-    
-    renderCampsite(campsite) {
-        return (
-            <div className="col-md-5 m-1">
-                <Card>
-                    <CardImg top src={campsite.image} alt={campsite.name} />
-                    <CardBody>
-                        <CardTitle>{campsite.name}</CardTitle>
-                        <CardText>{campsite.description}</CardText>
-                    </CardBody>
-                </Card>
-            </div>
-        );
-    }
+	renderCampsite(campsite) {
+		return (
+			<div className='col-md-5 m-1'>
+				<Card>
+					<CardImg top src={campsite.image} alt={campsite.name} />
+					<CardBody>
+						<CardTitle>{campsite.name}</CardTitle>
+						<CardText>{campsite.description}</CardText>
+					</CardBody>
+				</Card>
+			</div>
+		);
+	}
 
-    renderComments(comments) {
-        if(comments)
-            return (
-                <div className="col-md-5 m-1">
-                    <h4>Comments</h4>
-                    {comments.map(comment => {
-                        return(
-                            <div>
-                                <p>
-                                    {comment.text}
-                                    {comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))} 
-                                </p>
-                            </div>
-                        );
-                    }) 
-                    }
-                </div>
-            );
-            
-        return <div />;
-    }
+	renderComments(comments) {
+		if (comments)
+			return (
+				<div className='col-md-5 m-1'>
+					<h4>Comments</h4>
+					{comments.map((comment) => {
+						return (
+							<div>
+								<p>
+									{comment.text}
+									{comment.author},{" "}
+									{new Intl.DateTimeFormat("en-US", {
+										year: "numeric",
+										month: "short",
+										day: "2-digit",
+									}).format(new Date(Date.parse(comment.date)))}
+								</p>
+							</div>
+						);
+					})}
+				</div>
+			);
 
-    render () {
-        if (this.props.campsite) {
-            return (
-                <div className="row">
-                    <renderCampsite campsite={this.props.renderCampsite} />
-                </div>
-            ); 
-        return  (
-            <div />
-        );
-    }
-}
+		return <div />;
+	}
+
+	render() {
+		if (this.props.campsite) {
+			return (
+				<div className='row'>
+					{/* AMY INSTRUCTOR: renderCampsite() is not a component, it's a function. 
+                        the only time a function is a component, is when it is outside of the class component. 
+
+                        replace your code at line 56 with this:
+                        {this.renderCampsite(this.props.campsite)}
+                    */}
+
+					<renderCampsite campsite={this.props.renderCampsite} />
+
+					{/* AMY INSTRUCTOR: call your renderComment here  */}
+				</div>
+			);
+		}
+
+		// AMY INSTRUCTOR: you had your closing curly brace in the wrong spot. I fixed it for you to have it working.
+		return <div />;
+	}
 }
 
 export default CampsiteInfo;
