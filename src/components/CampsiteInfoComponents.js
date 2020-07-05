f224eee56672799c110f7b1c0d5c27de683f6e47
Edith Santiago <esantiago808@gmail.com>
2020-07-05 01:51:30 -0700

    Week 2 Workshop Task 3

--- src/components/CampsiteInfoComponents.js
+++ src/components/CampsiteInfoComponents.js
@@ -17,6 +17,28 @@ class CampsiteInfo extends Component {
         );
     }
 
+    renderComments(comments) {
+        if(comments)
+            return (
+                <div className="col-md-5 m-1">
+                    <h4>Comments</h4>
+                    {comments.map(comment => {
+                        return(
+                            <div>
+                                <p>
+                                    {comment.text}
+                                    {comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))} 
+                                </p>
+                            </div>
+                        );
+                    }) 
+                    }
+                </div>
+            );
+            
+        return <div />;
+    }
+
     render () {
         if (this.props.campsite) {
             return (
