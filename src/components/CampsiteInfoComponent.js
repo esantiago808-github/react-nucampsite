1c99e60413deb8783143f91c85e0286d57575c8a
Edith Santiago <esantiago808@gmail.com>
2020-07-21 21:29:19 -0700

    Week 5 Redux Thunk

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -14,19 +14,20 @@ import {
 } from "reactstrap";
 import { Link } from "react-router-dom";
 import { Control, LocalForm, Errors } from "react-redux-form";
+import { Loading } from './LoadingComponent';
 
 function RenderCampsite({ campsite }) {
-	return (
-		<div className='col-md-5 m-1'>
-			<Card>
-				<CardImg top src={campsite.image} alt={campsite.name} />
-				<CardBody>
-					<CardText>{campsite.description}</CardText>
-				</CardBody>
-			</Card>
-		</div>
-	);
-}
+		return (
+			<div className='col-md-5 m-1'>
+				<Card>
+					<CardImg top src={campsite.image} alt={campsite.name} />
+					<CardBody>
+						<CardText>{campsite.description}</CardText>
+					</CardBody>
+				</Card>
+			</div>
+		);
+	}
 
 function RenderComments({ comments, addComment, campsiteId }) {
 	if (comments)
@@ -59,6 +60,26 @@ function RenderComments({ comments, addComment, campsiteId }) {
 }
 
 function CampsiteInfo(props) {
+	if (props.isLoading) {
+		return (
+			<div className="container">
+				<div className="row">
+					<Loading />
+				</div>
+			</div>
+		);
+	}
+	if (props.errMess) {
+		return (
+			<div className="container">
+				<div className="row">
+					<div className="col">
+						<h4>{props.errMess}</h4>
+					</div>
+				</div>
+			</div>
+		);
+	}
 	if (props.campsite) {
 		return (
 			<div className='container'>
--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
+import { Loading } from './LoadingComponent';
 
 function Home(props) {
     return (
         <div className="container">
             <div className="row"> 
                 <div className="col-md m-1">
-                    <RenderCard item={props.campsite} />
+                    <RenderCard
+                        item={props.campsite}
+                        isLoading={props.campsitesLoading}
+                        errMess={props.campsitesErrMess}
+                    />
                 </div>
                 <div className="col-md m-1">
-                    <RenderCard item={props.promotion} />    
+                    <RenderCard 
+                        item={props.promotion}
+                    />    
                 </div>
                 <div className="col-md m-1">
                     <RenderCard item={props.partner} />
@@ -19,7 +26,17 @@ function Home(props) {
     );
 }
 
-function RenderCard({item}) {
+function RenderCard({item, isLoading, errMess}) {
+    if (isLoading) {
+        return (
+             <Loading />
+        );
+    }
+    if (errMess) {
+        return (
+            <h4>{errMess}</h4>
+        );
+    }    
     return (
         <Card>
             <CardImg src={item.image} alt={item.name} />
