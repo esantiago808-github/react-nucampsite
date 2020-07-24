1e33f558c01cb28a2691be851f3dd12fa2c4bdfa
Edith Santiago <esantiago808@gmail.com>
2020-07-24 15:22:41 -0700

    Week 5 Building and Deployment

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react';
 import { Breadcrumb, BreadcrumbItem, Button, Label, Col, Row } from 'reactstrap';
 import { Link } from 'react-router-dom';
-import { Control, Form, Errors, actions } from 'react-redux-form';
-import { render } from '@testing-library/react';
+import { Control, Form, Errors,} from 'react-redux-form';
 
 const required = val => val && val.length;
 const maxLength = len => val => !val || (val.length <= len);
 const minLength = len => val => val && (val.length >= len);
-const isNumber = val => !isNaN(+val);
 const validEmail = val => /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(val);
 
 class Contact extends Component {
@@ -94,8 +92,6 @@ class Contact extends Component {
     }
     
     render () {
-
-        const errors = this.validate(this.state.firstName, this.state.lastName, this.state.phoneNum, this.state.email);
 
         return (
             <div className="container">
